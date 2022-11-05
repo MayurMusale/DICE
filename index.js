fd9b0a95f6109ac7e4fc6492055cbb80d83f0afe
Mayur Musale <56197502+MayurMusale@users.noreply.github.com>
2022-11-05 13:58:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var num1 = Math.floor(Math.random() * 6 + 1);
 var num2 = Math.floor(Math.random() * 6 + 1);
 
 function checkScore() {
-    document.querySelector(".span1").innerHTML = `<img src='/images/dice${num1}.png' class="img-fluid"></img>`;
-    document.querySelector(".span2").innerHTML = `<img src='/images/dice${num2}.png' class="img-fluid"></img>`;
+    document.querySelector(".span1").innerHTML = `<img src='images/dice${num1}.png' class="img-fluid"></img>`;
+    document.querySelector(".span2").innerHTML = `<img src='images/dice${num2}.png' class="img-fluid"></img>`;
     if (num1 > num2) {
         document.querySelector(".Winmsg1").innerHTML = "Winner";
     }
